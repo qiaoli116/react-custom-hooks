e14eda686ad8e1380492660af65b920a2495a9f4
qiaoli116 <qiaoli.116@gmail.com>
2022-08-17 15:57:25 +1000

    verion 1.0.4

---
+++ index.js
@@ -0,0 +1,31 @@
+import React from "react";
+const useLifeCycle = ({
+    didMount = () =>{console.log("didMount undefined");}, 
+    willUpdate = () =>{console.log("willUpdate undefined");},
+    didUpdate = () =>{console.log("didUpdate undefined");}, 
+    willUnmount = () =>{console.log("willUnmount undefined");}
+}={}) => {
+    const isInitialMount = React.useRef(true);
+    const isUnmount = React.useRef(false);
+    React.useEffect(() => {
+        didMount();
+        return ()=>{
+            willUnmount();
+            isUnmount.current = true;
+        }; 
+    }, []);
+    
+    React.useEffect(() => {
+        if (isInitialMount.current) {
+            isInitialMount.current = false;
+        } else {
+            didUpdate();
+        }
+        return ()=>{
+            if(!isUnmount.current){
+                willUpdate();
+            };
+        } 
+    });
+}
+export {useLifeCycle};
